f0bd628b59089287bee32b15499e3f9683b3e57e
abatdorf505 <abatdorf505@gmail.com>
2025-03-12 00:03:22 -0400

    Write pseudocode for getting computer's choice

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Create function named getComputerChoice
+    // Create variable to hold the computer's choice
+    // Pick a random integer from 0 to 2
+    // If the integer is 0, the computer chooses rock
+    // If the integer is 1, the computer chooses paper
+    // If the integer is 2, the computer chooses scissors
+    // Return computer choice
+
+// Call getComputerChoice function
