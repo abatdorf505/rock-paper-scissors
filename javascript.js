246d4f684180b2f5bd7fc34aaede45660f3be451
abatdorf505 <abatdorf505@gmail.com>
2025-03-12 00:06:37 -0400

    Rewrite pseudo

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 // Create function named getComputerChoice
-    // Create variable to hold the computer's choice
-    // Pick a random integer from 0 to 2
-    // If the integer is 0, the computer chooses rock
-    // If the integer is 1, the computer chooses paper
-    // If the integer is 2, the computer chooses scissors
-    // Return computer choice
+    // Create variable to hold a random integer from 0 to 2
+    // If the integer is 0, return rock
+    // If the integer is 1, return paper
+    // If the integer is 2, return scissors
 
 // Call getComputerChoice function
