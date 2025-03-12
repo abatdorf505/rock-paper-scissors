a3fc5f37aca12d7c9ef672734b3ea8520cf0be13
abatdorf505 <abatdorf505@gmail.com>
2025-03-12 00:33:27 -0400

    Remove unneccessary calls

--- javascript.js
+++ javascript.js
@@ -25,11 +25,6 @@ function getHumanChoice() {
     }
 }
 
-// Call getComputerChoice function
-console.log(getComputerChoice());
-// Call getHumanChoice function
-console.log(getHumanChoice());
-
 // Create variable to store the user's score and initialize it at 0
 let humanScore = 0;
 // Create variable to store the computer's score and initialize it at 0
