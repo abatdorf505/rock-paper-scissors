ede651dbbb62fb389e53927dc185fac5a0b81353
abatdorf505 <abatdorf505@gmail.com>
2025-03-12 01:02:16 -0400

    Write game logic

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ function getComputerChoice() {
     // rock, paper, and scissors respectively.
     let computerChoice = Math.floor(Math.random() * 3);
     // Return computer choice
-    return computerChoice
+    return computerChoice;
 }
 
 // Create function named getHumanChoice
@@ -30,36 +30,55 @@ function getHumanChoice() {
     }
 }
 
-// Create function named playRound with parameters "humanChoice" & "computerChoice"
-function playRound() {
-    // Get string equivalent of human choice
-    humanChoiceString = (humanChoice === 0) ? "Rock" :
-    (humanChoice === 1) ? "Paper" : "Scissors";
-    // Get string equivalent of computer choice
-    computerChoiceString = (computerChoice === 0) ? "Rock": 
-    (computerChoice === 1) ? "Paper" : "Scissors";
+// Create function named playGame
+function playGame() {
+    // Create variable to store the user's score and initialize it at 0
+    let humanScore = 0;
+    // Create variable to store the computer's score and initialize it at 0
+    let computerScore = 0;
 
-    // Get difference of human and computer choice values and store in variable 
-    // called result
-    let result = humanChoice - computerChoice;
-    // If the difference is -2 or 1, the player wins
-    // Otherwise, the computer wins. Ties are not possible in individual rounds
-    if (result === -2 || result === 1) {
-        // Send message to console indiciating player success
-        // Increment player score value
-        console.log(`You win! ${humanChoiceString} beats ${computerChoiceString}`)
-        humanScore++;
+    // Create function named playRound with parameters "humanChoice" & 
+    // "computerChoice"
+    function playRound(humanChoice, computerChoice) {
+        // Get string equivalent of human choice
+        humanChoiceString = (humanChoice === 0) ? "Rock" :
+        (humanChoice === 1) ? "Paper" : "Scissors";
+        // Get string equivalent of computer choice
+        computerChoiceString = (computerChoice === 0) ? "Rock": 
+        (computerChoice === 1) ? "Paper" : "Scissors";
+
+        // Get difference of human and computer choice values and store in variable 
+        // called result
+        let result = humanChoice - computerChoice;
+        // If the difference is -2 or 1, the player wins
+        // Otherwise, the computer wins. Ties are not possible in individual rounds
+        if (result === -2 || result === 1) {
+            // Send message to console indiciating player success
+            // Increment player score value
+            console.log(`You win! ${humanChoiceString} beats ${computerChoiceString}`);
+            humanScore++;
+        } else {
+            // Send message to console indicating player loss
+            // Increment computer score value
+            console.log(`You lose! ${computerChoiceString} beats ${humanChoiceString}`);
+            computerScore++;
+        }
+    }
+
+    // Call getHumanChoice, getComputerChoice, and playRound 5 times
+    for(let index = 0; index < 5; index++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    // If the human scored more, declare them the winner
+    // If the computer scored more, declare them the winner.
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
     } else {
-        // Send message to console indicating player loss
-        // Increment computer score value
-        console.log(`You lose! ${computerChoiceString} beats ${humanChoiceString}`)
-        computerScore++;
+        console.log("You lost the game, sorry!");
     }
 }
 
-// Create function named playGame
-    // Create score variables
-    // Define playRound function
-    // Call playRound 5 times
-    // If the human scored more, declare them the winner
-    // If the computer scored more, declare them the winner.
+// Start a game
+playGame();
